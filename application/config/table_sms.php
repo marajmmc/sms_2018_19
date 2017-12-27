698fb115286fcbfb5507efb8d386fcccc8b61956
Saiful Islam <saifultitu90@gmail.com>
2017-12-27 11:32:00 +0600

    table_sms updated

--- application/config/table_sms.php
+++ application/config/table_sms.php
@@ -5,5 +5,6 @@ $config['table_sms_template'] = 'arm_sms_2018_19.sms_template';
 
 //Stock
 $config['table_sms_stock_in_variety'] = 'arm_sms_2018_19.sms_stock_in_variety';
+$config['table_sms_stock_in_variety_details'] = 'arm_sms_2018_19.sms_stock_in_variety_details';
 $config['table_sms_stock_out_variety'] = 'arm_sms_2018_19.sms_stock_out_variety';
 $config['table_sms_stock_summary_variety'] = 'arm_sms_2018_19.sms_stock_summary_variety';
