860eda534afd60965524bfc87e23c3d8fbaa8f77
Saiful Islam <saifultitu90@gmail.com>
2018-01-27 10:12:05 +0600

    barcode helper added in autoload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url','user','query','system','task');
+$autoload['helper'] = array('url','user','query','system','task','barcode');
 
 /*
 | -------------------------------------------------------------------
