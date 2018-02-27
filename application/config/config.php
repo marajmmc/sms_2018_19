9285d28cd9d7a31f8d8342cd3c073a1148b13ae5
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2018-02-27 16:54:41 +0600

    checking without .htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -36,7 +36,7 @@ $config['base_url'] = 'http://localhost/sms_2018_19/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
