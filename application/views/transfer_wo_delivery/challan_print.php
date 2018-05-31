181733a8ec3fbe0fa82219c6af1e12595ed45ce5
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2018-06-01 01:27:11 +0600

    all list added challan print button

--- application/views/transfer_wo_delivery/challan_print.php
+++ application/views/transfer_wo_delivery/challan_print.php
@@ -3,8 +3,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $CI=& get_instance();
 $action_buttons=array();
 $action_buttons[]=array(
-    'label'=>$CI->lang->line("ACTION_BACK"),
+    'label'=>$CI->lang->line("ACTION_BACK").' to Pending List',
     'href'=>site_url($CI->controller_url)
+);
+$action_buttons[]=array(
+    'label'=>$CI->lang->line("ACTION_BACK").' to All list',
+    'href'=>site_url($CI->controller_url.'/index/list_all')
 );
 if(isset($CI->permissions['action4']) && ($CI->permissions['action4']==1))
 {
