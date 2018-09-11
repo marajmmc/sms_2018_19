1afe373901014ef4d307ee530596cd5465851d96
Md. Shaiful Islam <islambuet@gmail.com>
2018-09-11 12:23:13 +0600

    Details analysis fixed

--- application/controllers/Report_stock_variety_details_analysis.php
+++ application/controllers/Report_stock_variety_details_analysis.php
@@ -331,11 +331,21 @@ class Report_stock_variety_details_analysis extends Root_Controller
             {
                 if($pack_size_id!=0)
                 {
-                    $stocks[$result['variety_id']][$result['pack_size_id']]=$this->initialize_row('','','',$pack_sizes[$result['pack_size_id']]);
+                    if($pack_size_id>0)
+                    {
+                        if($pack_size_id==$result['pack_size_id'])
+                        {
+                            $stocks[$result['variety_id']][$result['pack_size_id']]=$this->initialize_row('','','',$pack_sizes[$result['pack_size_id']]);
+                        }
+                    }
+                    else
+                    {
+                        $stocks[$result['variety_id']][$result['pack_size_id']]=$this->initialize_row('','','',$pack_sizes[$result['pack_size_id']]);
+                    }
+
                 }
             }
-
-            if(($pack_size_id>0)||($pack_size_id==-2))
+            if($pack_size_id==-2)
             {
                 $stocks[$result['variety_id']][$result['pack_size_id']]['opening_stock_pkt']+=$result['in_convert_bulk_pack_opening'];
                 $stocks[$result['variety_id']][$result['pack_size_id']]['opening_stock_kg']+=(($result['in_convert_bulk_pack_opening']*$pack_sizes[$result['pack_size_id']])/1000);
@@ -346,6 +356,20 @@ class Report_stock_variety_details_analysis extends Root_Controller
                 $stocks[$result['variety_id']][$result['pack_size_id']]['end_stock_pkt']+=($result['in_convert_bulk_pack_opening']+$result['in_convert_bulk_pack']);
                 $stocks[$result['variety_id']][$result['pack_size_id']]['end_stock_kg']+=((($result['in_convert_bulk_pack_opening']+$result['in_convert_bulk_pack'])*$pack_sizes[$result['pack_size_id']])/1000);
 
+            }
+            elseif($pack_size_id>0)
+            {
+                if($pack_size_id==$result['pack_size_id'])
+                {
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['opening_stock_pkt']+=$result['in_convert_bulk_pack_opening'];
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['opening_stock_kg']+=(($result['in_convert_bulk_pack_opening']*$pack_sizes[$result['pack_size_id']])/1000);
+
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['in_convert_bulk_pack_pkt']+=$result['in_convert_bulk_pack'];
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['in_convert_bulk_pack_kg']+=(($result['in_convert_bulk_pack']*$pack_sizes[$result['pack_size_id']])/1000);
+
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['end_stock_pkt']+=($result['in_convert_bulk_pack_opening']+$result['in_convert_bulk_pack']);
+                    $stocks[$result['variety_id']][$result['pack_size_id']]['end_stock_kg']+=((($result['in_convert_bulk_pack_opening']+$result['in_convert_bulk_pack'])*$pack_sizes[$result['pack_size_id']])/1000);
+                }
             }
             else if($pack_size_id==0)
             {
