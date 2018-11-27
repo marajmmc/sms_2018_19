14e91c51346fb1c619c46156b60888a12d72f40f
Md. Shaiful Islam <islambuet@gmail.com>
2018-11-28 00:00:49 +0600

    Image resize size,width,height included

--- application/views/main.php
+++ application/views/main.php
@@ -139,6 +139,9 @@ if($result)
             var display_date_format = "dd-M-yy";
             var SELECT_ONE_ITEM = "<?php echo $CI->lang->line('SELECT_ONE_ITEM'); ?>";
             var DELETE_CONFIRM = "<?php echo $CI->lang->line('DELETE_CONFIRM'); ?>";
+            var SYSTEM_IMAGE_SIZE_TO_RESIZE=409600;//1372022=1.3mb,409600=400KB
+            var SYSTEM_IMAGE_MAX_WIDTH=400;
+            var SYSTEM_IMAGE_MAX_HEIGHT=300;
             var resized_image_files=[];
             var system_crops=JSON.parse('<?php echo json_encode($system_crops);?>');
             var system_types=JSON.parse('<?php echo json_encode($system_types);?>');
