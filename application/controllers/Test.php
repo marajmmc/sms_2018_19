cd566a50cb3ba1f7fe24fe7e60701291eeb99232
Md. Shaiful Islam <islambuet@gmail.com>
2018-12-05 18:17:10 +0600

    Test controller for lc month_id fixing

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -5,7 +5,30 @@ class Test extends CI_Controller {
 
     public function index()
     {
-        echo date('n',1539799200);
+        $source_tables=array(
+            'lc'=>'arm_sms_2018_19.sms_lc_open'
+
+        );
+        $destination_tables=array(
+            'lc'=>'arm_sms_2018_19.sms_lc_open'
+        );
+        $results=Query_helper::get_info($source_tables['lc'],'*',array());
+        $total=0;
+        foreach($results as $result)
+        {
+            $month_id=date('n',$result['date_opening']);
+            if($month_id!=$result['month_id'])
+            {
+                $total++;
+                $data=array();
+                $data['month_id'] = $month_id;
+                Query_helper::update($destination_tables['lc'],$data, array('id='.$result['id']), false);
+
+                //echo $result['id'].'-'.$month_id.'-'.$result['month_id'].'<br>';
+            }
+        }
+        echo $total.'updated';
+
     }
 
 }
